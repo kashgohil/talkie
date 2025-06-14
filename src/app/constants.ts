962c9a37b314a02f55e210a4480b7922cbaca280
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-06-15 03:06:36 +0530

    [chore] models and mode constants

---
+++ src/app/constants.ts
@@ -0,0 +1,47 @@
+export const models = [
+	{
+		id: "gpt-4o",
+		name: "GPT-4o",
+		provider: "openai",
+	},
+	{
+		id: "gpt-4o-mini",
+		name: "GPT-4o-mini",
+		provider: "openai",
+	},
+	{
+		id: "claude-3-5-sonnet",
+		name: "Claude 3.5 Sonnet",
+		provider: "anthropic",
+	},
+	{
+		id: "claude-3-5-haiku",
+		name: "Claude 3.5 Haiku",
+		provider: "anthropic",
+	},
+	{
+		id: "claude-3-5-opus",
+		name: "Claude 3.5 Opus",
+		provider: "anthropic",
+	},
+	{
+		id: "claude-3-7-sonnet",
+		name: "Claude 3.7 Sonnet",
+		provider: "anthropic",
+	},
+];
+
+export const modes = [
+	{
+		id: "chat",
+		name: "Chat",
+	},
+	{
+		id: "code",
+		name: "Code",
+	},
+	{
+		id: "search",
+		name: "Search",
+	},
+];
