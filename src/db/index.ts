50409a3be43ea962a85d36c65f638c7275b27dfd
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-06-16 01:42:11 +0530

    [chore] move db init to db directory

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,10 @@
 import { drizzle } from "drizzle-orm/libsql";
+import * as schema from "./schema";
 
 export const db = drizzle({
+	schema: {
+		...schema,
+	},
 	connection: {
 		url: process.env.TURSO_DATABASE_URL!,
 		authToken: process.env.TURSO_AUTH_TOKEN!,
