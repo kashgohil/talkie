b04cddeec4aad9752e69f7f11fa8bebb0170ecb9
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-06-16 01:47:53 +0530

    [projects] fetch and render projects

--- src/components/nav-projects.tsx
+++ src/components/nav-projects.tsx
@@ -1,80 +1,93 @@
-"use client";
+import { Edit, Folder, Trash2 } from "lucide-react";
 
-import { Folder, MoreHorizontal, Share, Trash2, type LucideIcon } from "lucide-react";
-
-import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuItem,
-	DropdownMenuSeparator,
-	DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+import { getUserProjects } from "@/app/actions";
 import {
 	SidebarGroup,
 	SidebarGroupLabel,
 	SidebarMenu,
-	SidebarMenuAction,
 	SidebarMenuButton,
 	SidebarMenuItem,
-	useSidebar,
 } from "@/components/ui/sidebar";
+import { AddProject } from "./add-project";
+import { Button } from "./ui/button";
+import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
 
-export function NavProjects({
-	projects,
-}: {
-	projects: {
-		name: string;
-		url: string;
-		icon: LucideIcon;
-	}[];
-}) {
-	const { isMobile } = useSidebar();
+async function ProjectsList() {
+	const userProjects = await getUserProjects();
+	if (!userProjects?.length) {
+		return <div className="flex items-center justify-center h-full">No projects yet.</div>;
+	}
 
 	return (
-		<SidebarGroup className="group-data-[collapsible=icon]:hidden">
-			<SidebarGroupLabel className="text-wisteria-500">Projects</SidebarGroupLabel>
-			<SidebarMenu>
-				{projects.map((item) => (
-					<SidebarMenuItem key={item.name}>
-						<SidebarMenuButton
-							asChild
-							className="group/menu-item"
-						>
-							<a href={item.url}>
-								<item.icon className="text-wisteria-500 group-hover/menu-item:text-accent-foreground" />
-								<span>{item.name}</span>
+		<SidebarMenu>
+			{userProjects.map((item) => (
+				<SidebarMenuItem key={item.id}>
+					<SidebarMenuButton
+						asChild
+						className="group/menu-item"
+					>
+						<div className="flex items-center justify-between">
+							<a href={`/project/${item.id}`}>
+								<div className="flex items-center justify-between gap-2">
+									<Folder
+										size={16}
+										className="text-wisteria-500 group-hover/menu-item:text-accent-foreground"
+									/>
+									<span className="truncate">{item.name}</span>
+								</div>
 							</a>
-						</SidebarMenuButton>
-						<DropdownMenu>
-							<DropdownMenuTrigger asChild>
-								<SidebarMenuAction showOnHover>
-									<MoreHorizontal />
-									<span className="sr-only">More</span>
-								</SidebarMenuAction>
-							</DropdownMenuTrigger>
-							<DropdownMenuContent
-								className="w-48"
-								side={isMobile ? "bottom" : "right"}
-								align={isMobile ? "end" : "start"}
-							>
-								<DropdownMenuItem className="group/menu-item">
-									<Folder className="text-wisteria-500 group-hover/menu-item:text-accent-foreground" />
-									<span>View Project</span>
-								</DropdownMenuItem>
-								<DropdownMenuItem className="group/menu-item">
-									<Share className="text-wisteria-500 group-hover/menu-item:text-accent-foreground" />
-									<span>Share Project</span>
-								</DropdownMenuItem>
-								<DropdownMenuSeparator />
-								<DropdownMenuItem className="group/menu-item">
-									<Trash2 className="text-wisteria-500 group-hover/menu-item:text-accent-foreground" />
-									<span>Delete Project</span>
-								</DropdownMenuItem>
-							</DropdownMenuContent>
-						</DropdownMenu>
-					</SidebarMenuItem>
-				))}
-			</SidebarMenu>
+							<div className="items-center absolute z-10 -right-full group-hover/menu-item:right-0 bg-wisteria-200 p-1 shadow-2xl transition-all duration-300 rounded-lg group-hover/menu-item:flex delay-300">
+								<Tooltip>
+									<TooltipTrigger asChild>
+										<Button
+											variant="ghost"
+											size="icon"
+										>
+											<Edit
+												size={12}
+												className="text-wisteria-500 group-hover/menu-item:text-accent-foreground"
+											/>
+										</Button>
+									</TooltipTrigger>
+									<TooltipContent>
+										<span>Edit</span>
+										<span className="sr-only">Edit</span>
+									</TooltipContent>
+								</Tooltip>
+								<Tooltip>
+									<TooltipTrigger asChild>
+										<Button
+											variant="ghost"
+											size="icon"
+										>
+											<Trash2
+												size={12}
+												className="text-wisteria-500 group-hover/menu-item:text-accent-foreground"
+											/>
+										</Button>
+									</TooltipTrigger>
+									<TooltipContent>
+										<span>Delete</span>
+										<span className="sr-only">Delete</span>
+									</TooltipContent>
+								</Tooltip>
+							</div>
+						</div>
+					</SidebarMenuButton>
+				</SidebarMenuItem>
+			))}
+		</SidebarMenu>
+	);
+}
+
+export async function NavProjects() {
+	return (
+		<SidebarGroup className="group-data-[collapsible=icon]:hidden">
+			<SidebarGroupLabel className="text-wisteria-500 flex items-center gap-2 justify-between">
+				<span>Projects</span>
+				<AddProject />
+			</SidebarGroupLabel>
+			<ProjectsList />
 		</SidebarGroup>
 	);
 }
