de3a45cc2de013400e082f62bcd1e16d5cfea61e
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-06-17 00:39:52 +0530

    [chore] updated the component name

---
+++ src/components/delete-project.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Trash2 } from "lucide-react";
+import { ConfirmationDialog } from "./confirmation-dialog";
+import { Button } from "./ui/button";
+
+export function DeleteProjectButton({
+	projectId,
+	onDelete,
+}: {
+	projectId: string;
+	onDelete: (projectId: string) => void;
+}) {
+	return (
+		<ConfirmationDialog
+			title="Delete Project"
+			description="Are you sure you want to delete this project?"
+			action="Delete"
+			cancel="Cancel"
+			onAction={() => onDelete(projectId)}
+		>
+			<Button
+				variant="ghost"
+				size="icon"
+				className="!p-1"
+			>
+				<Trash2
+					size={8}
+					className="text-wisteria-500 group-hover/menu-item:text-accent-foreground"
+				/>
+			</Button>
+		</ConfirmationDialog>
+	);
+}
