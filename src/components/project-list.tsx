df2e959e41744f97ba5ca36ed6195b0a75d485b2
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-06-21 16:15:31 +0400

    [chore] style changes

--- src/components/project-list.tsx
+++ src/components/project-list.tsx
@@ -2,8 +2,11 @@
 
 import { deleteProject } from "@/app/actions";
 import { projects } from "@/db/schema";
+import { cn } from "@/lib/utils";
 import { InferSelectModel } from "drizzle-orm";
 import { Edit, Folder } from "lucide-react";
+import Link from "next/link";
+import { useParams } from "next/navigation";
 import { useCallback, useOptimistic, useTransition } from "react";
 import { DeleteProjectButton } from "./delete-project";
 import { Button } from "./ui/button";
@@ -11,6 +14,8 @@ import { SidebarMenuButton, SidebarMenuItem } from "./ui/sidebar";
 
 export function ProjectList(props: { projects: InferSelectModel<typeof projects>[] }) {
 	const { projects } = props;
+	const params = useParams();
+	const projectId = params.projectId as string;
 
 	const [, startTransition] = useTransition();
 
@@ -35,18 +40,24 @@ export function ProjectList(props: { projects: InferSelectModel<typeof projects>
 		<SidebarMenuItem key={item.id}>
 			<SidebarMenuButton
 				asChild
-				className="group/menu-item overflow-y-visible"
+				className={cn(
+					"group/menu-item [&_svg]:text-accent hover:[&_svg]:text-accent-foreground",
+					projectId === item.id && "bg-accent/20"
+				)}
 			>
 				<div className="w-full flex items-center justify-between relative">
-					<a href={`/project/${item.id}`}>
+					<Link
+						href={`/project/${item.id}`}
+						className="w-full"
+					>
 						<div className="flex items-center justify-between gap-2">
 							<Folder
 								size={16}
-								className="text-wisteria-500 group-hover/menu-item:text-accent-foreground"
+								className="flex-shrink-0"
 							/>
-							<span className="truncate">{item.name}</span>
+							<span className="flex-1 truncate">{item.name}</span>
 						</div>
-					</a>
+					</Link>
 					<div className="items-center absolute z-10 -right-full group-hover/menu-item:right-0 bg-wisteria-200 p-1 shadow-2xl transition-all duration-300 rounded-lg group-hover/menu-item:flex">
 						<Button
 							variant="ghost"
