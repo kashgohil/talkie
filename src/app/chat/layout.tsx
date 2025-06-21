4f18389cea267f3b387ee29b822c2067130db174
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-06-21 16:13:02 +0400

    [chore] style and metadata changes

--- src/app/chat/layout.tsx
+++ src/app/chat/layout.tsx
@@ -2,15 +2,22 @@ import { AppSidebar } from "@/components/app-sidebar";
 import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import React from "react";
 
+export async function generateMetadata() {
+	return {
+		title: "Wisteria - AI Chat",
+		description: "Chat with AI like never before",
+	};
+}
+
 export default function ChatLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
 	return (
-		<SidebarProvider>
+		<SidebarProvider className="h-screen">
 			<AppSidebar />
-			<SidebarInset className="flex flex-col p-4">
+			<SidebarInset>
 				<SidebarTrigger className="-ml-1 absolute top-4 left-4" />
 				{children}
 			</SidebarInset>
