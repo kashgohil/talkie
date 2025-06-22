673e18887061ecef6ffd8858f744cf0eb19f1fff
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-06-22 14:42:15 +0400

    [chore] remove rewrites

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	/* config options here */
-	rewrites: async () => {
-		return [
-			{
-				source: "/chat/:chatID",
-				destination: "/chat?id=:chatID",
-			},
-		];
-	},
 };
 
 export default nextConfig;
