539625034b7a9c0c7e6874d85e244d2a05430b0f
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-06-22 14:45:17 +0400

    [chore] new chat

--- src/components/nav-history.tsx
+++ src/components/nav-history.tsx
@@ -1,12 +1,13 @@
 import { getUserChats } from "@/app/actions";
 import { SidebarGroup, SidebarGroupLabel, SidebarMenu } from "@/components/ui/sidebar";
+import { Plus } from "lucide-react";
+import Link from "next/link";
+import { Button } from "./ui/button";
 import { ChatList } from "./ui/chat-list";
+import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
 
 async function History() {
 	const userChats = await getUserChats();
-	if (!userChats?.length) {
-		return <div className="flex p-2 text-sm items-center justify-center h-full">No chats yet.</div>;
-	}
 
 	return (
 		<SidebarMenu>
@@ -19,7 +20,24 @@ export async function NavHistory() {
 	return (
 		<SidebarGroup className="group-data-[collapsible=icon]:hidden">
 			<SidebarGroupLabel className="text-wisteria-500 flex items-center gap-2 justify-between">
-				<span>History</span>
+				<span>Chats</span>
+				<Link href="/chat">
+					<Tooltip>
+						<TooltipTrigger asChild>
+							<Button
+								size="icon"
+								variant="ghost"
+								className="!p-1"
+							>
+								<Plus />
+							</Button>
+						</TooltipTrigger>
+						<TooltipContent>
+							<span>New Chat</span>
+							<span className="sr-only">New Chat</span>
+						</TooltipContent>
+					</Tooltip>
+				</Link>
 			</SidebarGroupLabel>
 			<History />
 		</SidebarGroup>
