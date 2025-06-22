6c6a3a438c82ef539da530fd81bf3a23b3acf01d
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-06-22 14:44:47 +0400

    [chore] delete chat

--- src/components/delete-chat.tsx
+++ src/components/delete-chat.tsx
@@ -1,28 +1,20 @@
 "use client";
 
-import { Trash2 } from "lucide-react";
+import { useState } from "react";
 import { ConfirmationDialog } from "./confirmation-dialog";
-import { Button } from "./ui/button";
 
 export function DeleteChatButton({ chatId, onDelete }: { chatId: string; onDelete: (chatId: string) => void }) {
+	const [isOpen, setIsOpen] = useState(false);
+
 	return (
 		<ConfirmationDialog
+			open={isOpen}
+			onOpenChange={setIsOpen}
 			title="Delete Chat"
 			description="Are you sure you want to delete this chat?"
 			action="Delete"
 			cancel="Cancel"
 			onAction={() => onDelete(chatId)}
-		>
-			<Button
-				variant="ghost"
-				size="icon"
-				className="!p-1"
-			>
-				<Trash2
-					size={8}
-					className="text-wisteria-500 group-hover/menu-item:text-accent-foreground"
-				/>
-			</Button>
-		</ConfirmationDialog>
+		/>
 	);
 }
